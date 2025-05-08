578df5746f283f970f4202ea92d5cd2f84894094
zhaoxifan <179988305@qq.com>
2025-05-08 11:10:06 +0800

    add LMMatterCore.framework

---
+++ LMMatterCore.framework/Headers/LMMatterCore.h
@@ -0,0 +1,18 @@
+//
+//  LMMatterCore.h
+//  LMMatterCore
+//
+//  Created by Charles on 2023/4/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LMMatterCore.
+FOUNDATION_EXPORT double LMMatterCoreVersionNumber;
+
+//! Project version string for LMMatterCore.
+FOUNDATION_EXPORT const unsigned char LMMatterCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LMMatterCore/PublicHeader.h>
+
+
